7a013fcfd35ab190b5afaac96faaea9c5dd03949
caligraf <obriard@hotmail.com>
2020-11-08 23:07:59 +0100

    Fix bug when delete AdressBook element
    - only one confirmation popup showed
    - can delete when "All Address Books" selected

--- chrome/content/confirmbeforedelete/addressbook.js
+++ chrome/content/confirmbeforedelete/addressbook.js
@@ -1,5 +1,7 @@
 // Import any needed modules.
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var {
+    Services
+} = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 // Load an additional JavaScript file.
 Services.scriptloader.loadSubScript("chrome://confirmbeforedelete/content/confirmbeforedelete/CBD-common.js", window, "UTF-8");
@@ -9,31 +11,33 @@ if (!CBD)
 
 function onLoad(activatedWhileWindowOpen) {
     window.CBD.init();
-    
+
     // Address Book
     if (typeof window.AbDelete != "undefined" && typeof AbDeleteOrig == "undefined") {
         var AbDeleteOrig = window.AbDelete;
         window.AbDelete = function () {
-            var selectedDir = window.GetSelectedDirectory();
-            var isList = window.GetDirectoryFromURI(selectedDir).isMailList
-                var types = window.GetSelectedCardTypes();
-            if (types == window.kNothingSelected)
+            let types = window.GetSelectedCardTypes();
+            if (types == window.kNothingSelected) {
                 return;
+            }
+
             var enableConfirm = window.CBD.prefs.getBoolPref("extensions.confirmbeforedelete.addressbook.enable");
-            var param = isList ? "contactyesno2" : "contactyesno";
-            if (types == window.kCardsOnly && enableConfirm && !window.CBD.confirmbeforedelete(param))
+            if (!enableConfirm) {
+                // Delete cards from address books or mailing lists.
+                window.gAbView.deleteSelectedCards();
                 return;
+            }
+
             AbDeleteOrig.apply(this, arguments);
         };
     }
-    
 }
 
 function onUnload(deactivatedWhileWindowOpen) {
-  // Cleaning up the window UI is only needed when the
-  // add-on is being deactivated/removed while the window
-  // is still open. It can be skipped otherwise.
-  if (!deactivatedWhileWindowOpen) {
-    return
-  }
+    // Cleaning up the window UI is only needed when the
+    // add-on is being deactivated/removed while the window
+    // is still open. It can be skipped otherwise.
+    if (!deactivatedWhileWindowOpen) {
+        return
+    }
 }
