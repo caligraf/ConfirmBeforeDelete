2cb2cf9d0842d3171d83ff99fa88791d7ac75742
Tooru Fujisawa <arai_a@mac.com>
2023-07-01 03:27:27 +0900

    Use Services global variable if possible
    Services.jsm is planned to be removed in Firefox 117 cycle in https://bugzilla.mozilla.org/show_bug.cgi?id=1780695 .
    Services global variable is available in WebExtensions experiments API global
    from version 88 https://bugzilla.mozilla.org/show_bug.cgi?id=1698158 ,
    and also available in all system globals from version 104
    https://bugzilla.mozilla.org/show_bug.cgi?id=1667455 ,
    and those code doesn't have to import Services.jsm for recent versions.

--- chrome/content/confirmbeforedelete/messenger.js
+++ chrome/content/confirmbeforedelete/messenger.js
@@ -1,5 +1,7 @@
 // Import any needed modules.
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var Services = globalThis.Services || ChromeUtils.import(
+  "resource://gre/modules/Services.jsm"
+).Services;
 var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
 
 // Load an additional JavaScript file.
